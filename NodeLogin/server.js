1f03c70ab1114b96e5e614a5d46c11980bd6d90f
dhanush-sys <dhanushsivasamyg@gmail.com>
2020-04-12 21:28:51 +0530

    danny commiting

--- NodeLogin/server.js
+++ NodeLogin/server.js
@@ -1,16 +1,8 @@
 const express=require("express");
 const app=express();
-var mysql = require('mssql');
-var dbconfig = require('./Database/database');
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); 
 const PORT=4000;
-/*exSign=require("./signuplogin/exportersign");
-farmSign=require("./signuplogin/farmersign")
-login=require("./signuplogin/loginuser")
-app.use('/',exSign());
-app.use('/',farmSign());
-app.use('/',login())*/
 signupLogin=require("./signuplogin");
-app.use('/',signupLogin);
+app.use('/',signupLogin());
 app.listen(PORT);
